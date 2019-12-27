fff39caea55c033fd2352879f1b4b7a4f99ea37f
ohadrau <ohadrau@yahoo.com>
2019-12-27 12:00:55 -0500

    Remove dependency on Batteries

--- tokenize.ml
+++ tokenize.ml
@@ -1,4 +1,6 @@
-open Batteries
+let slice ~first ~last string =
+  let length = last - first in
+  String.sub string first length
 
 (* Split a string into an AppendList of strings, chopping at every newline and
    removing the newline characters. *)
@@ -7,13 +9,13 @@ let split_on_newline string =
   let first = ref 0 in
   for i = 0 to String.length string - 1 do
     if string.[i] = '\n' then begin
-      Queue.add (String.slice ~first:!first ~last:i string)lines;
+      Queue.add (slice ~first:!first ~last:i string) lines;
       first := i + 1
     end
   done;
   (* Append the remaining characters after the last split *)
   if !first < String.length string - 1 then begin
-    Queue.add (String.slice ~first:!first ~last:(String.length string) string) lines 
+    Queue.add (slice ~first:!first ~last:(String.length string) string) lines 
   end;
   lines
 
@@ -27,22 +29,22 @@ let split_tokens string =
     | '\n' | ' ' | '\t' ->
       (* Push everything before this character (if there is anything) *)
       if !first <> i then begin
-        Queue.add (String.slice ~first:!first ~last:i string) tokens
+        Queue.add (slice ~first:!first ~last:i string) tokens
       end;
       (* Then push this character *)
-      Queue.add (String.slice ~first:i ~last:(i+1) string) tokens;
+      Queue.add (slice ~first:i ~last:(i+1) string) tokens;
       (* And skip it *)
       first := i + 1
     | '-' ->
       (* When a word is already hyphenated, split it into tokens so we don't needlessly
          rehyphenate it and end up with a double hyphen *)
-      Queue.add (String.slice ~first:!first ~last:(i+1) string) tokens;
+      Queue.add (slice ~first:!first ~last:(i+1) string) tokens;
       first := i + 1
     | _ -> ()
   done;
   (* Append the remaining characters after the last split *)
   if !first < String.length string - 1 then begin
-    Queue.add (String.slice ~first:!first ~last:(String.length string) string) tokens
+    Queue.add (slice ~first:!first ~last:(String.length string) string) tokens
   end;
   tokens
 
